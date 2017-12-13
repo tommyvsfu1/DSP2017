1f12ceed31ed727fbcbcee72d985bb5f6042950f
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-12-13 19:11:39 +0800

    Program structure

---
+++ hw3/test_env/mydisambig.cpp
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+/* Usage:																*/
+/* ./disambig –text $file –map $map –lm $LM –order $order > $output		*/
+
+int main ( argc[], argv[] ) {
+
+	// check arguments
+
+	// read in files: text, map(candidate), lm(prob), order(2 for bigram)
+
+	// for every sentence, read the map and list the candidate
+
+	// do Viterbi
+
+	// Add <s> </s> and print
+
+
+	return 0;
+}
