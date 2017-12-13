a0606069f1e93e1d25c974246e60c8078849574f
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-12-13 21:28:14 +0800

    Read in the files

--- hw3/mydisambig.cpp
+++ hw3/mydisambig.cpp
@@ -2,21 +2,24 @@
 # include <string.h>
 
 #include "option.h"
+#include "File.h"
+#include "Ngram.h"
+#include "Vocab.h"
+#include "VocabMap.h"
 
 /* Usage:																*/
 /* ./disambig -text $file -map $map -lm $LM -order $order > $output		*/
-/* argv[0]	  1     2     3    4    5   6   7      8					*/
 
 static unsigned order = 2;
-static char *lmFile = 0;
-static char *mapFile = 0;
-static char *textFile = 0;
+static char *lmFileName = 0;
+static char *mapFileName = 0;
+static char *textFileName = 0;
 
 static Option options[] = {
-    { OPT_STRING, "lm", &lmFile, "hidden token sequence model" },
+    { OPT_STRING, "lm", &lmFileName, "hidden token sequence model" },
+    { OPT_STRING, "map", &mapFileName, "mapping from observable to hidden tokens" },
+    { OPT_STRING, "text", &textFileName, "text file to disambiguate" },
     { OPT_UINT, "order", &order, "ngram order to use for lm" },
-    { OPT_STRING, "map", &mapFile, "mapping from observable to hidden tokens" },
-    { OPT_STRING, "text", &textFile, "text file to disambiguate" },
 };
 
 int main ( int argc, char **argv ) {
@@ -24,16 +27,34 @@ int main ( int argc, char **argv ) {
 	// parse options
 	Opt_Parse(argc, argv, options, 4, 0);
 
-	printf("text: %s, map: %s, lm: %s, order: %d\n", textFile, mapFile, lmFile, order);
-
 	// read in files: text, map(candidate), lm(prob), order(2 for bigram)
+	File lmFile(lmFileName, "r");
+	File mapFile(mapFileName, "r");
+	File textFile(textFileName, "r");
 
-	// for every sentence, read the map and list the candidate
+	Vocab Big5, ZhuYin;
+	VocabMap map(ZhuYin, Big5);
+	map.read(mapFile);
+	mapFile.close();
 
-	// do Viterbi
+	Ngram lm(ZhuYin, order);
+	lm.read(lmFile);
+	lmFile.close();
 
-	// Add <s> </s> and print
+	char *line;
+	while( line = textFile.getline() ) {
 
+		// list the candidate
+
+		// do Viterbi
+
+		// Trace back
+
+		// Add <s> </s> and print
+
+
+	}
+	textFile.close();
 
 	return 0;
 }
